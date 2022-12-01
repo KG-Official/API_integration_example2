5605735dfc3c2e03a756ed354f76df88348381e2
kashif <kgofficial786@gmail.com>
2022-12-01 16:43:38 +0500

    "today update"

--- app/src/main/java/com/example/api_intergration_example2/MainActivity.kt
+++ app/src/main/java/com/example/api_intergration_example2/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.api_intergration_example2
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.api_intergration_example2.databinding.ActivityMainBinding
 import com.example.api_intergration_example2.models.Adapter
@@ -19,40 +21,53 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.await
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var adapter: Adapter
     lateinit var binding: ActivityMainBinding
-    private var data = MutableLiveData<List<Post1>>()
+    private var data = mutableListOf <Post1>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getData()
+        binding.btnChangeActivity.setOnClickListener{
+            intent = Intent(applicationContext,MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 
 
     fun getData() {
 
-        CoroutineScope(IO).launch {
+     CoroutineScope(IO).launch {
+            delay(3000)
+
+            Log.d("MainActivity","Coroutine is running")
             var responce = DataService.Instance.getData()
             var body = responce.body()
 
             if (body != null) {
-                data.postValue(body.data.post1s)
+                data.addAll(body.data.post1s)
 
-                withContext(Main){
-                    data.observe(this@MainActivity, Observer {
-                        adapter = Adapter(this@MainActivity, it)
-                        binding.recyclerView.adapter = adapter
-                        binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                    })
+                withContext(Main) {
+                    Log.d("MainActivity","Coroutine is still running")
+                    adapter = Adapter(this@MainActivity, data)
+                    binding.recyclerView.adapter = adapter
+                    binding.recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
                 }
             }
         }
-    }
+
+  }
+
+
 
 
 }
